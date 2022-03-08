42f23d7480497b1787212c5a2cae387af8285ba4
Marcos Cianci <marcos.lopescianci@nttdata.com>
2022-03-08 18:10:51 -0300

    Create output.tf - terraform modules

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,24 @@
 # Date: Seg 7 Mar 2022
 # Terraform Module - AWS RDS
 
+### AWS RDS ###
+
+output "rds_hostname" {
+
+    description = "RDS instance hostname"
+    value       = aws_db_instance.rds.address
+    sensitive   = true
+}
+
+output "db_parameter_group" {
+
+    description = "Name DB Parameter Group"
+    value = aws_db_parameter_group.db_parameter_group.name
+}
+
+#output "db_subnet_group_name" {
+#
+#    description = ""
+#    value = aws_db_subnet_group_name.db_subnet_group_name.name
+#  
+#}
